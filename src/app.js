e3559d73033ac9d295fa3b47b0a90a4a53a875fd
Andrey Korovin <misticwonder@gmail.com>
2017-07-16 17:01:20 +0300

    reformat

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(helmet());
 app.use(compress());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', feathers.static(app.get('public')));
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
